6a0d581e9781472d509343c0eeaede196d736299
DIMITA <dimitriomorales01@gmail.com>
2023-10-12 22:56:06 +0200

    customisation du service d'insertion pour emettre sur message

--- src/pixel/pixel.gateway.ts
+++ src/pixel/pixel.gateway.ts
@@ -2,17 +2,27 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { PixelService } from './pixel.service';
 import { CreatePixelDto } from './dto/create-pixel.dto';
+import { Server } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class PixelGateway {
+  @WebSocketServer()
+  server: Server;
   constructor(private readonly pixelService: PixelService) {}
 
   @SubscribeMessage('createPixel')
-  create(@MessageBody() createPixelDto: CreatePixelDto) {
-    return this.pixelService.create(createPixelDto);
+  async create(@MessageBody() createPixelDto: CreatePixelDto) {
+    const message = await this.pixelService.create(createPixelDto);
+    this.server.emit('message', message);
+    return message;
   }
 
   @SubscribeMessage('findAllPixel')
