deb8a56174357f657c70c4d3e1d8c35d46c3b394
DIMITA <dimitriomorales01@gmail.com>
2023-10-12 19:31:16 +0200

    ajout de typeorm, les différents package de socket et un essai

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pixel war API')
+    .setDescription('The Pixel war API description')
+    .setVersion('1.0')
+    .addTag('pixelWar')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
